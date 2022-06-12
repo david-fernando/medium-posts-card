3a2f0286638edbe82eb385d8422b7bcc1e98b547
david-fernando <davidfernandodamata21@gmail.com>
2022-06-12 13:31:40 -0300

    Add style on Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,29 @@
+import '../style/index.css'
+import styles from '../style/Card.module.css'
+
 function Card(){
   return (
-    <h1>Card</h1>
+    <div className={styles.container}>
+      <span>
+        <img
+         className={styles.thumbnail} 
+         src="https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ"
+         alt="Título do artigo"
+        />
+      </span>
+      <span className={styles.content}>
+        <h3>Título do artigo</h3>
+        <p>
+        is simply dummy text of the printing and typesetting industry.
+        </p>
+        <p>
+          12.06.2022
+        </p>
+        <p>
+          dark-mode js-tutorial javascript
+        </p>
+      </span>
+    </div>
   )
 }
 
