378364793fc9eca2d89d6ef76745b4fbbd23cdb9
david-fernando <davidfernandodamata21@gmail.com>
2022-06-15 01:50:56 -0300

    Remove classnames bind

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,6 @@
-import classnames from 'classnames/bind';
+import classnames from 'classnames';
 
 import styles from '../style/Card.module.css'
-
-const classBind = classnames.bind(styles)
 
 function Card({ borderRadius = true, showTags = false, showDate = false }){
   const borderRadiusContainer = (borderRadius) && styles.borderRadiusContainer
@@ -10,29 +8,29 @@ function Card({ borderRadius = true, showTags = false, showDate = false }){
   const imageUrl = 'https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ'
   
   return (
-    <div className={classBind(styles.container, borderRadiusContainer)}>
+    <div className={classnames(styles.container, borderRadiusContainer)}>
       <span>
         <img
-         className={classBind(styles.thumbnail, borderRadiusThumbnail)} 
+         className={classnames(styles.thumbnail, borderRadiusThumbnail)} 
          src={imageUrl}
          alt="Título do artigo"
         />
       </span>
-      <span className={classBind(styles.content)}>
-        <span className={classBind(styles.title)}>Título do artigo</span>
-        <p className={classBind(styles.description)}>
+      <span className={styles.content}>
+        <span className={styles.title}>Título do artigo</span>
+        <p className={styles.description}>
           Um pequeno tutorial de como fazer dark theme com HTML, CSS e JavaScript puro.
         </p>
         {
           (showDate) && (
-            <p className={classBind(styles.date)}>
+            <p className={styles.date}>
               12.06.2022
             </p>
           )
         }
         {
           (showTags) && (
-            <p className={classBind(styles.tags)}>
+            <p className={styles.tags}>
               dark-mode js-tutorial javascript
             </p>
           )
