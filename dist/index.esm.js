c18277daed553b55e673964d2794b15ecdabff4d
david-fernando <davidfernandodamata21@gmail.com>
2022-06-15 18:07:01 -0300

    Update index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1,36 +1,11 @@
 import classnames from 'classnames';
 import { jsxs, jsx } from 'react/jsx-runtime';
 
-function styleInject(css, ref) {
-  if ( ref === void 0 ) ref = {};
-  var insertAt = ref.insertAt;
+var e=[],t=[];function n(n,r){if(n&&"undefined"!=typeof document){var a,s=!0===r.prepend?"prepend":"append",d=!0===r.singleTag,i="string"==typeof r.container?document.querySelector(r.container):document.getElementsByTagName("head")[0];if(d){var u=e.indexOf(i);-1===u&&(u=e.push(i)-1,t[u]={}),a=t[u]&&t[u][s]?t[u][s]:t[u][s]=c();}else a=c();65279===n.charCodeAt(0)&&(n=n.substring(1)),a.styleSheet?a.styleSheet.cssText+=n:a.appendChild(document.createTextNode(n));}function c(){var e=document.createElement("style");if(e.setAttribute("type","text/css"),r.attributes)for(var t=Object.keys(r.attributes),n=0;n<t.length;n++)e.setAttribute(t[n],r.attributes[t[n]]);var a="prepend"===s?"afterbegin":"beforeend";return i.insertAdjacentElement(a,e),e}}
 
-  if (!css || typeof document === 'undefined') { return; }
-
-  var head = document.head || document.getElementsByTagName('head')[0];
-  var style = document.createElement('style');
-  style.type = 'text/css';
-
-  if (insertAt === 'top') {
-    if (head.firstChild) {
-      head.insertBefore(style, head.firstChild);
-    } else {
-      head.appendChild(style);
-    }
-  } else {
-    head.appendChild(style);
-  }
-
-  if (style.styleSheet) {
-    style.styleSheet.cssText = css;
-  } else {
-    style.appendChild(document.createTextNode(css));
-  }
-}
-
-var css_248z = ".Card-module_container__SodM9 {\r\n  width: min-content;\r\n  height: min-content;\r\n  min-width: 20rem;\r\n  max-width: 22rem;\r\n  min-height: 20rem;\r\n  max-height: 22rem;\r\n  flex-direction: column;\r\n  display: flex;\r\n  background: #fff;\r\n  border: 1px solid rgba(0,0,0,.2);\r\n  font-size: 10px;\r\n  font-family: 'Roboto', sans-serif;\r\n  margin: 0;\r\n}\r\n\r\n.Card-module_borderRadiusContainer__37hyI {\r\n  border-radius: 10px;\r\n}\r\n\r\n.Card-module_thumbnail__GicTK {\r\n  width: 100%;\r\n  height: 12.533rem;\r\n}\r\n\r\n.Card-module_borderRadiusThumbnail__m-SrW {\r\n  border-top-left-radius: 10px;\r\n  border-top-right-radius: 10px;\r\n}\r\n\r\n.Card-module_content__7fty0 {\r\n  padding: 10px;\r\n}\r\n\r\n.Card-module_title__otTtq, .Card-module_description__v-qMr, .Card-module_date__mbUlM, .Card-module_tags__yIEry, .Card-module_readMore__ld-WG {\r\n  font-size: 1rem;\r\n  color: #53565b\r\n}\r\n\r\n.Card-module_title__otTtq {\r\n  font-weight: 700;\r\n}\r\n\r\n.Card-module_description__v-qMr {\r\n  text-align: initial;\r\n  max-height: 44%;\r\n  text-overflow: ellipsis;\r\n  -webkit-box-orient: vertical;\r\n  -webkit-line-clamp: 2;\r\n  display: -webkit-box;\r\n  overflow: hidden;\r\n}\r\n\r\n.Card-module_date__mbUlM, .Card-module_tags__yIEry, .Card-module_readMore__ld-WG {\r\n  font-size: 0.8rem;\r\n}\r\n\r\n.Card-module_tags__yIEry {\r\n  word-spacing: 0.5rem;\r\n}";
-var styles = {"container":"Card-module_container__SodM9","borderRadiusContainer":"Card-module_borderRadiusContainer__37hyI","thumbnail":"Card-module_thumbnail__GicTK","borderRadiusThumbnail":"Card-module_borderRadiusThumbnail__m-SrW","content":"Card-module_content__7fty0","title":"Card-module_title__otTtq","description":"Card-module_description__v-qMr","date":"Card-module_date__mbUlM","tags":"Card-module_tags__yIEry","readMore":"Card-module_readMore__ld-WG"};
-styleInject(css_248z);
+var css = ".Card_module_container__845f97ef {\r\n  width: min-content;\r\n  height: min-content;\r\n  min-width: 20rem;\r\n  max-width: 22rem;\r\n  min-height: 20rem;\r\n  max-height: 22rem;\r\n  flex-direction: column;\r\n  display: flex;\r\n  background: #fff;\r\n  border: 1px solid rgba(0,0,0,.2);\r\n  font-size: 10px;\r\n  font-family: 'Roboto', sans-serif;\r\n  margin: 0;\r\n}\r\n\r\n.Card_module_borderRadiusContainer__845f97ef {\r\n  border-radius: 10px;\r\n}\r\n\r\n.Card_module_thumbnail__845f97ef {\r\n  width: 100%;\r\n  height: 12.533rem;\r\n}\r\n\r\n.Card_module_borderRadiusThumbnail__845f97ef {\r\n  border-top-left-radius: 10px;\r\n  border-top-right-radius: 10px;\r\n}\r\n\r\n.Card_module_content__845f97ef {\r\n  padding: 10px;\r\n}\r\n\r\n.Card_module_title__845f97ef, .Card_module_description__845f97ef, .Card_module_date__845f97ef, .Card_module_tags__845f97ef, .Card_module_readMore__845f97ef {\r\n  font-size: 1rem;\r\n  color: #53565b\r\n}\r\n\r\n.Card_module_title__845f97ef {\r\n  font-weight: 700;\r\n}\r\n\r\n.Card_module_description__845f97ef {\r\n  text-align: initial;\r\n  max-height: 44%;\r\n  text-overflow: ellipsis;\r\n  -webkit-box-orient: vertical;\r\n  -webkit-line-clamp: 2;\r\n  display: -webkit-box;\r\n  overflow: hidden;\r\n}\r\n\r\n.Card_module_date__845f97ef, .Card_module_tags__845f97ef, .Card_module_readMore__845f97ef {\r\n  font-size: 0.8rem;\r\n}\r\n\r\n.Card_module_tags__845f97ef {\r\n  word-spacing: 0.5rem;\r\n}";
+var modules_e5a68879 = {"container":"Card_module_container__845f97ef","borderRadiusContainer":"Card_module_borderRadiusContainer__845f97ef","thumbnail":"Card_module_thumbnail__845f97ef","borderRadiusThumbnail":"Card_module_borderRadiusThumbnail__845f97ef","content":"Card_module_content__845f97ef","title":"Card_module_title__845f97ef","description":"Card_module_description__845f97ef","date":"Card_module_date__845f97ef","tags":"Card_module_tags__845f97ef","readMore":"Card_module_readMore__845f97ef"};
+n(css,{});
 
 function Card(_ref) {
   var _ref$borderRadius = _ref.borderRadius,
@@ -39,30 +14,30 @@ function Card(_ref) {
       showTags = _ref$showTags === void 0 ? false : _ref$showTags,
       _ref$showDate = _ref.showDate,
       showDate = _ref$showDate === void 0 ? false : _ref$showDate;
-  var borderRadiusContainer = borderRadius && styles.borderRadiusContainer;
-  var borderRadiusThumbnail = borderRadius && styles.borderRadiusThumbnail;
+  var borderRadiusContainer = borderRadius && modules_e5a68879.borderRadiusContainer;
+  var borderRadiusThumbnail = borderRadius && modules_e5a68879.borderRadiusThumbnail;
   var imageUrl = 'https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ';
   return /*#__PURE__*/jsxs("div", {
-    className: classnames(styles.container, borderRadiusContainer),
+    className: classnames(modules_e5a68879.container, borderRadiusContainer),
     children: [/*#__PURE__*/jsx("span", {
       children: /*#__PURE__*/jsx("img", {
-        className: classnames(styles.thumbnail, borderRadiusThumbnail),
+        className: classnames(modules_e5a68879.thumbnail, borderRadiusThumbnail),
         src: imageUrl,
         alt: "T\xEDtulo do artigo"
       })
     }), /*#__PURE__*/jsxs("span", {
-      className: styles.content,
+      className: modules_e5a68879.content,
       children: [/*#__PURE__*/jsx("span", {
-        className: styles.title,
+        className: modules_e5a68879.title,
         children: "T\xEDtulo do artigo"
       }), /*#__PURE__*/jsx("p", {
-        className: styles.description,
+        className: modules_e5a68879.description,
         children: "Um pequeno tutorial de como fazer dark theme com HTML, CSS e JavaScript puro."
       }), showDate && /*#__PURE__*/jsx("p", {
-        className: styles.date,
+        className: modules_e5a68879.date,
         children: "12.06.2022"
       }), showTags && /*#__PURE__*/jsx("p", {
-        className: styles.tags,
+        className: modules_e5a68879.tags,
         children: "dark-mode js-tutorial javascript"
       })]
     })]
