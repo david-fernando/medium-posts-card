ae76ae7cd45873bc4eedb02b85970c3af599bbc1
david-fernando <davidfernandodamata21@gmail.com>
2022-06-17 19:54:38 -0300

    Add rollup-plugin-typescript2

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from '@rollup/plugin-babel';
 import external from 'rollup-plugin-peer-deps-external';
 import del from 'rollup-plugin-delete';
 import pkg from './package.json';
+import typescript from "rollup-plugin-typescript2";
 import styles from "rollup-plugin-styles";
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -18,6 +19,7 @@ export default {
             babelHelpers: 'bundled' 
         }),
         del({ targets: ['dist/*'] }),
+        typescript({ useTsconfigDeclarationDir: true }),
         styles({ modules: true })
     ],
     external: Object.keys(pkg.peerDependencies || {}),
