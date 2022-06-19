18b68ade2833e142782dfc323a05b8e9d57fba4d
david-fernando <davidfernandodamata21@gmail.com>
2022-06-19 12:26:23 -0300

    Add interface.ts

---
+++ src/interface/interface.ts
@@ -0,0 +1,38 @@
+interface Options {
+  options?: {
+    borderRadius?: boolean,
+    showTags?: boolean,
+    showDate?: boolean
+  }
+}
+
+interface User {
+  username: string
+}
+
+interface Props {
+  username: User["username"],
+  options?: Options["options"]
+}
+
+interface UserData {
+  data: {
+    title: string,
+    date: string,
+    image: string,
+    description: string,
+    tags: Array<string>
+  }
+}
+
+interface CardProps {
+  userdata: UserData["data"],
+  options?: Options["options"]
+}
+
+export {
+  Props,
+  Options,
+  User,
+  CardProps
+}
