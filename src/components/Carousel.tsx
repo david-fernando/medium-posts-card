6d45d05c2ef64530b9cd8ffa9737abcf93e17253
david-fernando <davidfernandodamata21@gmail.com>
2022-06-22 14:09:07 -0300

    Add useCarousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,34 +1,22 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import classnames from 'classnames';
 import { GrFormPrevious, GrFormNext } from 'react-icons/gr'
 import { useIsVisible } from 'react-is-visible'
 import { Props } from '../interface/interface'
 import Card from './Card'
 import useFetch from '../hooks/useFetch';
+import useCarousel from '../hooks/useCarousel';
 
 import styles from '../style/Carousel.module.css'
 
-
 function Carousel({ username, options = {} }: Props){
-  const [ moveRight, setMoveRight ] = useState(0)
+  const { moveForward, moveBack, moveRight } = useCarousel()
 
   const { dataMedium } = useFetch(`https://mediumpostapi.herokuapp.com/?usermedium=${username}`)
   const openInNewTab = (options.hasOwnProperty('openInNewTab'))? options.openInNewTab : true
   const nameTarget = (openInNewTab)? '_blank' : '_self'
   const cardContainer = useRef()
   const cardIsVisible = useIsVisible(cardContainer)
-
-  function moveForward() {
-    if(!cardIsVisible){
-      setMoveRight(moveRight + 21)
-    }
-  }
-
-  function moveBack() {
-    if(moveRight > 0){
-      setMoveRight(moveRight - 21)
-    }
-  }
 
   return (
     <div className={styles.container}>
@@ -45,7 +33,7 @@ function Carousel({ username, options = {} }: Props){
         }
         <span ref={cardContainer} style={{width:'0rem', height: '0rem'}}></span>
       </span>
-      <button onClick={() => moveForward()} className={classnames(styles.carouselButton, styles.nextButton)} >
+      <button onClick={() => moveForward(cardIsVisible)} className={classnames(styles.carouselButton, styles.nextButton)} >
         <GrFormNext className={styles.iconButton} size={16} />
       </button>
     </div>
