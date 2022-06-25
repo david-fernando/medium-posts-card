7b461c0853db518e389872523a06502eab8b0703
david-fernando <davidfernandodamata21@gmail.com>
2022-06-24 22:51:57 -0300

    Add useIsVisible

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,11 +1,11 @@
 import { useRef } from 'react';
 import classnames from 'classnames';
 import { GrFormPrevious, GrFormNext } from 'react-icons/gr'
-import { useIsVisible } from 'react-is-visible'
 import { Props } from '../interface/interface'
 import Card from './Card'
 import useFetch from '../hooks/useFetch';
 import useCarousel from '../hooks/useCarousel';
+import useIsVisible from '../hooks/useIsVisible';
 
 import styles from '../style/Carousel.module.css'
 
@@ -15,23 +15,30 @@ function Carousel({ username, options = {} }: Props){
   const { dataMedium } = useFetch(`https://mediumpostapi.herokuapp.com/?usermedium=${username}`)
   const openInNewTab = (options.hasOwnProperty('openInNewTab'))? options.openInNewTab : true
   const nameTarget = (openInNewTab)? '_blank' : '_self'
-  const cardContainer = useRef()
-  const cardIsVisible = useIsVisible(cardContainer)
+  const cardContainer:any = useRef()
+  const carouselContainer:any = useRef()
+  const cardIsVisible:boolean = useIsVisible(carouselContainer ,cardContainer)
+  const numberOfCards = dataMedium.length - 1
+
+  const handleLastCard = (cardItem: number) => {
+    const lastCard = (cardItem === numberOfCards)? cardContainer  : null
+
+    return lastCard
+  }
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={carouselContainer} >
       <button onClick={() => moveBack()} className={classnames(styles.carouselButton, styles.previousButton)} >
         <GrFormPrevious className={styles.iconButton} size={16} />
       </button>
       <span className={styles.content} style={{right: `${moveRight}rem`, transition: 'right 0.6s linear' }} >
         {
           dataMedium.map((item: any, index: number) => (
-            <a href={item.link} target={nameTarget} key={index} >
+            <a href={item.link} ref={handleLastCard(index)} target={nameTarget} key={index} >
               <Card userdata={item} options={options} />
             </a>
           ))
         }
-        <span ref={cardContainer} style={{width:'0rem', height: '0rem'}}></span>
       </span>
       <button onClick={() => moveForward(cardIsVisible)} className={classnames(styles.carouselButton, styles.nextButton)} >
         <GrFormNext className={styles.iconButton} size={16} />
