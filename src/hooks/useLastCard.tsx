052909241846653762b3014c4df18ce49b8dc56d
david-fernando <davidfernandodamata21@gmail.com>
2022-06-24 23:07:48 -0300

    Add useLastCard.tsx

---
+++ src/hooks/useLastCard.tsx
@@ -0,0 +1,15 @@
+function useLastCard(){
+
+  function returnLastCard(cardItem: number, dataMedium: any[], element: any){
+    const numberOfCards = dataMedium.length - 1
+    const lastCard = (cardItem === numberOfCards)? element  : null
+  
+    return lastCard
+  }
+
+  return {
+    returnLastCard
+  }
+}
+
+export default useLastCard
