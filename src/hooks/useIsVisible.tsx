ae5315e1f368525f6d0b12a1ee860a046cc7c339
david-fernando <davidfernandodamata21@gmail.com>
2022-06-24 22:51:34 -0300

    Add useIsVisible.tsx

---
+++ src/hooks/useIsVisible.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+function useIsVisible(container: any, element: any){
+  const [ isVisible, setIsVisible ] = useState(false)
+  const [ elementPosition, setElementPosition ] = useState(0)
+  const [ containerWidth, setContainerWidth ] = useState(0)
+
+  const elementDefaultValue = { x: 0 }
+  const containerDefaultValue = { width: 0 }
+
+  setTimeout(()=>{
+    const { x } = element.current?.getBoundingClientRect?.() || elementDefaultValue
+    setElementPosition(x)
+    const { width } = container.current?.getBoundingClientRect?.() || containerDefaultValue
+    setContainerWidth(width)
+  }, 20)
+
+  useEffect(()=>{
+    determineIfElementIsVisible()
+  }, [elementPosition])
+
+  function determineIfElementIsVisible(){
+    const isElementVisible =  (elementPosition < containerWidth)? true : false
+
+    setIsVisible(isElementVisible)
+  }
+
+  return isVisible
+}
+
+export default useIsVisible
