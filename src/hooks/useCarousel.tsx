fae44f45126f2849db80ccb8d2b3be8bedee6685
david-fernando <davidfernandodamata21@gmail.com>
2022-06-27 19:18:58 -0300

    Add cardWidth const

--- src/hooks/useCarousel.tsx
+++ src/hooks/useCarousel.tsx
@@ -3,15 +3,17 @@ import { useState } from 'react'
 function useCarousel(){
   const [ moveRight, setMoveRight ] = useState(0)
 
+  const cardWidth = 19
+
   function moveForward(cardIsVisible: boolean) {
     if(!cardIsVisible){
-      setMoveRight(moveRight + 21)
+      setMoveRight(moveRight + cardWidth)
     }
   }
 
   function moveBack() {
     if(moveRight > 0){
-      setMoveRight(moveRight - 21)
+      setMoveRight(moveRight - cardWidth)
     }
   }
 
