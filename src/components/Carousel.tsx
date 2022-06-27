3bed8e5a9940ff394321c3a2c453f162dd9ddabb
david-fernando <davidfernandodamata21@gmail.com>
2022-06-27 19:03:34 -0300

    Add conditional view to carousel buttons

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -23,9 +23,13 @@ function Carousel({ username, options = {} }: Props){
 
   return (
     <div className={styles.container} ref={carouselContainer} >
-      <button onClick={() => moveBack()} className={classnames(styles.carouselButton, styles.previousButton)} >
-        <GrFormPrevious className={styles.iconButton} size={16} />
-      </button>
+      {
+        (moveRight > 0) && (
+          <button onClick={() => moveBack()} className={classnames(styles.carouselButton, styles.previousButton)} >
+            <GrFormPrevious className={styles.iconButton} size={16} />
+          </button>
+        )
+      }
       <span className={styles.content} style={{right: `${moveRight}rem`, transition: 'right 0.6s linear' }} >
         {
           dataMedium.map((item: any, index: number) => (
@@ -35,9 +39,13 @@ function Carousel({ username, options = {} }: Props){
           ))
         }
       </span>
-      <button onClick={() => moveForward(cardIsVisible)} className={classnames(styles.carouselButton, styles.nextButton)} >
-        <GrFormNext className={styles.iconButton} size={16} />
-      </button>
+      {
+        (!cardIsVisible) && (
+          <button onClick={() => moveForward(cardIsVisible)} className={classnames(styles.carouselButton, styles.nextButton)} >
+            <GrFormNext className={styles.iconButton} size={16} />
+          </button>
+        )
+      }
     </div>
   )
 }
