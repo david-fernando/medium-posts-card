dc342538d82b0e6cf67723b43beb64d5c4bf17fb
david-fernando <davidfernandodamata21@gmail.com>
2022-07-11 17:04:28 -0300

    Add useArray

--- src/hooks/useGetMedium.tsx
+++ src/hooks/useGetMedium.tsx
@@ -1,31 +1,27 @@
-import { useState, useCallback } from 'react';
-import useFetch from "use-http";
+import { useState } from 'react';
+import axios from 'axios'
 import useIsomophicEffect from "./useIsomophicEffect";
+import useArray from './useArray';
 import mockMedium from '../mocks/dataMedium';
 
 function useGetMedium(username: string){
   const [dataMedium, setDataMedium]: Array<any> = useState([]);
+  const { array } = useArray()
   const environment = process.env.NODE_ENV
   const isItaTestEnvironment = environment === 'test'
   const urlBase = 'https://mediumpostapi.herokuapp.com'
 
-  const fetchOptions:any = { cachePolicy: "no-cache" };
-  const { get } = useFetch(
-    `${urlBase}/?usermedium=${username}`,
-    fetchOptions
-  );
-
-  const getMedium = useCallback(async () => {
-    if(isItaTestEnvironment){
-      setDataMedium(mockMedium.data.dataMedium)
-      return;
-    }
-    const data = await get("");
-    setDataMedium(data.dataMedium);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [get]);
+  const getMedium = async () => {
+    const response = await axios.get(`${urlBase}/?usermedium=${username}`);
+    const data = response.data.dataMedium
+    setDataMedium(data);
+  }
 
   useIsomophicEffect(() => {
+    if(isItaTestEnvironment && array(dataMedium).isEmpty ){
+        setDataMedium(mockMedium.data.dataMedium)
+      return;
+    }
     getMedium();
   }, [getMedium]);
 
