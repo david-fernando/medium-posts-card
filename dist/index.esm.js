6244f941d0a237b335e238f99961f98ea853fb2e
david-fernando <davidfernandodamata21@gmail.com>
2022-07-21 22:54:04 -0300

    Update index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2,7 +2,7 @@ import { jsxs, jsx } from 'react/jsx-runtime';
 import { useState, useEffect, useRef } from 'react';
 import classnames from 'classnames';
 import { GrFormPrevious, GrFormNext } from 'react-icons/gr';
-import axios from 'axios';
+import axios$1 from 'axios';
 
 /******************************************************************************
 Copyright (c) Microsoft Corporation.
@@ -70,6 +70,7 @@ function __generator(thisArg, body) {
 
 function useArray() {
     function array(array) {
+        if (array === void 0) { array = []; }
         var isEmpty = (array.length === 0) ? true : false;
         return {
             isEmpty: isEmpty
@@ -101,99 +102,33 @@ function Card(_a) {
     return (jsxs("div", __assign({ className: classnames(modules_e5a68879.container, borderRadiusContainer) }, { children: [jsx("span", { children: jsx("img", { className: classnames(modules_e5a68879.thumbnail, borderRadiusThumbnail), src: imageUrl, alt: userdata.title }) }), jsxs("span", __assign({ className: modules_e5a68879.content }, { children: [jsx("span", __assign({ className: modules_e5a68879.title }, { children: userdata.title })), jsx("p", __assign({ className: modules_e5a68879.description }, { children: userdata.description })), (options.showDate) && (jsx("p", __assign({ className: modules_e5a68879.date }, { children: userdata.date }))), (options.showTags) && (jsx("p", __assign({ className: modules_e5a68879.tags }, { children: tagsWithBlankSpace })))] }))] })));
 }
 
-var mockMedium = {
-    'data': {
-        'dataMedium': [
-            {
-                title: "Como fazer dark theme com JavaScript puro",
-                date: "2021-03-07",
-                link: "https://medium.com/david-fernando/como-dark-theme-com-javascript-puro-fc277377447c?source=rss-e1120fb0abef------2",
-                image: "https://cdn-images-1.medium.com/max/1024/1*pUi3vkj06Vqp_sXeiI-UbQ.jpeg",
-                description: "Um pequeno tutorial de como fazer dark theme com HTML, CSS e JavaScript puro.",
-                tags: [
-                    "dark-mode",
-                    "js-tutorial",
-                    "javascript",
-                    "tutorial"
-                ]
-            },
-            {
-                title: "Como reduzir a quantidade de IFs",
-                date: "2020-09-25",
-                link: "https://medium.com/david-fernando/como-reduzir-a-quantidade-de-ifs-4484fc728397?source=rss-e1120fb0abef------2",
-                image: "https://cdn-images-1.medium.com/max/1024/1*6wlQhci1Pot4BWUPDpHbfw.jpeg",
-                description: "Uma introdução ao design pattern Strategy com JavaScript",
-                tags: [
-                    "strategy-design-pattern",
-                    "ecmascript",
-                    "javascript",
-                    "design-patterns",
-                    "ecmascript-6"
-                ]
-            },
-            {
-                title: "Por que usar TypeScript?",
-                date: "2020-09-24",
-                link: "https://medium.com/david-fernando/por-que-usar-typescript-ca15607eed33?source=rss-e1120fb0abef------2",
-                image: "https://cdn-images-1.medium.com/max/1024/1*ODf4X51nKEMElimXA706gQ.jpeg",
-                description: "Veja quais são os benefícios de utiliza-lo em seus projetos",
-                tags: [
-                    "ecmascript-2020",
-                    "typescript",
-                    "ecmascript-6",
-                    "javascript",
-                    "ecmascript"
-                ]
-            }
-        ]
-    }
-};
-
-function useGetMedium(username, ssr) {
-    var _this = this;
+function useGetMedium(username) {
     var _a = useState([]), dataMedium = _a[0], setDataMedium = _a[1];
     var array = useArray().array;
-    var environment = process.env.NODE_ENV;
-    var isItaTestEnvironment = environment === 'test';
-    var urlBase = 'https://mediumpostapi.herokuapp.com';
-    var getMedium = function () { return __awaiter(_this, void 0, void 0, function () {
-        var response, data;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    if (!array(dataMedium).isEmpty) {
+    function fetchMedium() {
+        return __awaiter(this, void 0, void 0, function () {
+            var baseUrl, response, data;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        baseUrl = 'https://mediumpostapi.herokuapp.com';
+                        return [4 /*yield*/, axios$1.get("".concat(baseUrl, "/?usermedium=").concat(username))];
+                    case 1:
+                        response = _a.sent();
+                        data = response.data.dataMedium;
+                        setDataMedium(data);
                         return [2 /*return*/];
-                    }
-                    if (isItaTestEnvironment) {
-                        setDataMedium(mockMedium.data.dataMedium);
-                        return [2 /*return*/];
-                    }
-                    return [4 /*yield*/, axios.get("".concat(urlBase, "/?usermedium=").concat(username))];
-                case 1:
-                    response = _a.sent();
-                    data = response.data.dataMedium;
-                    setDataMedium(data);
-                    return [2 /*return*/];
-            }
+                }
+            });
         });
-    }); };
-    if (ssr) {
-        (function () { return __awaiter(_this, void 0, void 0, function () { return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, getMedium()];
-                case 1: return [2 /*return*/, _a.sent()];
-            }
-        }); }); })();
     }
     useEffect(function () {
-        if (ssr) {
+        if (!array(dataMedium).isEmpty) {
             return;
         }
-        getMedium();
-    }, [getMedium]);
-    return {
-        dataMedium: dataMedium
-    };
+        fetchMedium();
+    }, [fetchMedium]);
+    return dataMedium;
 }
 
 function useCarousel() {
@@ -257,17 +192,38 @@ var modules_d1f32e6e = {"container":"Carousel_module_container__00d20a78","conte
 n(css,{});
 
 function Carousel(_a) {
-    var username = _a.username, _b = _a.options, options = _b === void 0 ? {} : _b;
-    var ssr = (options === null || options === void 0 ? void 0 : options.ssr) || false;
-    var _c = useCarousel(), moveForward = _c.moveForward, moveBack = _c.moveBack, position = _c.position;
-    var dataMedium = useGetMedium(username, ssr).dataMedium;
+    var _b = _a.username, username = _b === void 0 ? '' : _b, dataMedium = _a.dataMedium, _c = _a.options, options = _c === void 0 ? {} : _c;
+    // const ssr = options?.ssr || false
+    // const context = useContext(ServerSideContext)
+    var array = useArray().array;
+    var data = useGetMedium(username);
+    var medium = (array(data).isEmpty) ? dataMedium === null || dataMedium === void 0 ? void 0 : dataMedium.dataMedium : data;
+    var _d = useCarousel(), moveForward = _d.moveForward, moveBack = _d.moveBack, position = _d.position;
     var openInNewTab = (options.hasOwnProperty('openInNewTab')) ? options.openInNewTab : true;
     var nameTarget = (openInNewTab) ? '_blank' : '_self';
     var cardContainer = useRef();
     var carouselContainer = useRef();
     var cardIsVisible = useIsVisible(carouselContainer, cardContainer);
     var returnLastCard = useLastCard().returnLastCard;
-    return (jsxs("div", __assign({ className: modules_d1f32e6e.container, ref: carouselContainer }, { children: [(position > 0) && (jsx("button", __assign({ onClick: function () { return moveBack(); }, className: classnames(modules_d1f32e6e.carouselButton, modules_d1f32e6e.previousButton) }, { children: jsx(GrFormPrevious, { className: modules_d1f32e6e.iconButton, size: 24 }) }))), jsx("span", __assign({ className: modules_d1f32e6e.content, style: { right: "".concat(position, "rem"), transition: 'right 0.6s linear' } }, { children: dataMedium.map(function (item, index) { return (jsx("a", __assign({ href: item.link, ref: returnLastCard(index, dataMedium, cardContainer), target: nameTarget }, { children: jsx(Card, { userdata: item, options: options }) }), index)); }) })), (!cardIsVisible) && (jsx("button", __assign({ onClick: function () { return moveForward(cardIsVisible); }, className: classnames(modules_d1f32e6e.carouselButton, modules_d1f32e6e.nextButton) }, { children: jsx(GrFormNext, { className: modules_d1f32e6e.iconButton, size: 24 }) })))] })));
+    return (jsxs("div", __assign({ className: modules_d1f32e6e.container, ref: carouselContainer }, { children: [(position > 0) && (jsx("button", __assign({ onClick: function () { return moveBack(); }, className: classnames(modules_d1f32e6e.carouselButton, modules_d1f32e6e.previousButton) }, { children: jsx(GrFormPrevious, { className: modules_d1f32e6e.iconButton, size: 24 }) }))), jsx("span", __assign({ className: modules_d1f32e6e.content, style: { right: "".concat(position, "rem"), transition: 'right 0.6s linear' } }, { children: medium.map(function (item, index) { return (jsx("a", __assign({ href: item.link, ref: returnLastCard(index, medium, cardContainer), target: nameTarget }, { children: jsx(Card, { userdata: item, options: options }) }), index)); }) })), (!cardIsVisible) && (jsx("button", __assign({ onClick: function () { return moveForward(cardIsVisible); }, className: classnames(modules_d1f32e6e.carouselButton, modules_d1f32e6e.nextButton) }, { children: jsx(GrFormNext, { className: modules_d1f32e6e.iconButton, size: 24 }) })))] })));
 }
 
-export { Carousel as default };
+var axios = require('axios');
+function fetchMedium(username) {
+    return __awaiter(this, void 0, void 0, function () {
+        var baseUrl, response, data;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    baseUrl = 'https://mediumpostapi.herokuapp.com';
+                    return [4 /*yield*/, axios.get("".concat(baseUrl, "/?usermedium=").concat(username))];
+                case 1:
+                    response = _a.sent();
+                    data = response.data;
+                    return [2 /*return*/, data];
+            }
+        });
+    });
+}
+
+export { Carousel, fetchMedium };
