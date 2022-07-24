4b608cba547bde4294de161bf390ad67cc813689
david-fernando <davidfernandodamata21@gmail.com>
2022-07-23 22:14:20 -0300

    Remove fetchMedium local function

--- src/hooks/useGetMedium.tsx
+++ src/hooks/useGetMedium.tsx
@@ -1,25 +1,19 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios'
 import useArray from './useArray';
-
+import fetchMedium from '../utils/fetchMedium';
 
 function useGetMedium(username?: string, ssr?: boolean){
   const [dataMedium, setDataMedium]:Array<any> = useState([]);
   const { array } = useArray()
 
-  async function fetchMedium(){
-    const baseUrl = 'https://mediumpostapi.herokuapp.com'
-  
-    const response = await axios.get(`${baseUrl}/?usermedium=${username}`)
-    const data = response.data.dataMedium
-    setDataMedium(data)
-  }
-
   useEffect(() => {
     if(!array(dataMedium).isEmpty || ssr){
       return;
     }
-    fetchMedium()
+    (async()=>{
+      const data = await fetchMedium(username)
+      setDataMedium(data.dataMedium)
+    })()
   }, [fetchMedium]);
 
   return dataMedium
