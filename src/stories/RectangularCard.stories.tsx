86c03fc358ccb5eaafea2a85fb6e39283100ad3f
david-fernando <davidfernandodamata21@gmail.com>
2022-08-12 20:14:40 -0300

    Add RectangularCard.stories.tsx

---
+++ src/stories/RectangularCard.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import RectangularCard from '../components/RectangularCard'
+import cardData from "../mocks/cardData";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  component: RectangularCard
+}
+
+export const Basic = (args) => <RectangularCard {...args} />
+
+Basic.args = { 
+  userdata: cardData,
+  options: {
+    borderRadius: true,
+    showTags: false,
+    showDate: false
+  }
+}
