33106e2b7ad6411840f2d141bd11e341ddf12136
david-fernando <davidfernandodamata21@gmail.com>
2022-08-12 19:53:42 -0300

    Add cardData.tsx

---
+++ src/mocks/cardData.tsx
@@ -0,0 +1,14 @@
+const cardData =   {	
+  title: "Como fazer dark theme com JavaScript puro",
+  date: "2021-03-07",
+  image: "https://cdn-images-1.medium.com/max/1024/1*pUi3vkj06Vqp_sXeiI-UbQ.jpeg",
+  description: "Um pequeno tutorial de como fazer dark theme com HTML, CSS e JavaScript puro.",
+  tags: [
+   "dark-mode",
+   "js-tutorial",
+   "javascript",
+   "tutorial"
+  ]
+}
+
+export default cardData
