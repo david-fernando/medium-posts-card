5f7fbf6026918185544f3f08592697e4e130f378
david-fernando <davidfernandodamata21@gmail.com>
2022-08-15 20:04:29 -0300

    Add export List

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import Carousel from "./components/Carousel";
+import List from "./components/List";
 import fetchMedium from './utils/fetchMedium';
 
 export {
   Carousel,
+  List,
   fetchMedium
 }
