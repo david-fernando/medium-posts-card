2d8f7fc00aa4cd3fe78c6f62121c6a76eb92561f
david-fernando <davidfernandodamata21@gmail.com>
2022-08-15 20:24:13 -0300

    change title to Component SSR

--- src/utils/htmlStructure.ts
+++ src/utils/htmlStructure.ts
@@ -4,7 +4,7 @@ const html = (component) =>`<!DOCTYPE html>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Carousel SSR</title>
+  <title>Component SSR</title>
 </head>
 <body>
   <div id="app">
