bd4b24f7fa41d7a54788d472c637977e9f46a573
david-fernando <davidfernandodamata21@gmail.com>
2022-08-15 20:05:27 -0300

    Add list test

--- src/tests/ServerSideRedering.test.tsx
+++ src/tests/ServerSideRedering.test.tsx
@@ -4,9 +4,20 @@ describe('Rendering test of components on the server', () => {
 
   test('Should rendering carousel on server', async() => {
 
-    const classCardContainer = 'Card_module_container__0e637d59'
+    const classCardContainer = 'Card_module_container__864dde09'
     
     const test = await axios.get('http://localhost:3333/carousel')
+    
+    const classSelected = await test.data.match(classCardContainer)[0]
+
+    expect(classSelected).toBe(classCardContainer)
+  })
+
+  test('Should rendering list on server', async() => {
+
+    const classCardContainer = 'RectangularCard_module_container__0b2073a4'
+    
+    const test = await axios.get('http://localhost:3333/list')
     
     const classSelected = await test.data.match(classCardContainer)[0]
 
