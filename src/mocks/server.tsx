23241691acc2c1954c0d30a518e4da73c12ba8b1
david-fernando <davidfernandodamata21@gmail.com>
2022-08-15 20:04:53 -0300

    Add list route

--- src/mocks/server.tsx
+++ src/mocks/server.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
+import html from '../utils/htmlStructure'
 
-import { Carousel, fetchMedium } from '../../dist/index.esm.js'
+import { Carousel, List, fetchMedium } from '../../dist/index.esm.js'
 
 const handlers = [
   rest.get('http://localhost:3333/carousel', async(request, response, context)=>{
@@ -14,23 +15,21 @@ const handlers = [
       <Carousel dataMedium={data} options={{ssr: true}} />
     )
 
-    const html = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Carousel SSR</title>
-    </head>
-    <body>
-      <div id="app">
-        ${component}
-      </div>
-    </body>
-    </html>`
+    return response(
+      context.xml(html(component))
+    )
+  }),
+
+  rest.get('http://localhost:3333/list', async(request, response, context)=>{
+
+    const data = await fetchMedium('davidfernandodamata21')
+
+    const component = renderToString(
+      <List dataMedium={data} options={{ssr: true}} />
+    )
 
     return response(
-      context.xml(html)
+      context.xml(html(component))
     )
   })
 ]
