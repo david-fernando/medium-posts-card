0591964927ba9dfab83142d4f3d110f9aa43dcff
david-fernando <davidfernandodamata21@gmail.com>
2022-08-17 18:23:44 -0300

    Add Container

--- src/stories/List.stories.tsx
+++ src/stories/List.stories.tsx
@@ -7,10 +7,33 @@ export default {
 }
 
 const options = {
-  borderRadius: true,
-  showTags: false,
-  showDate: false,
+  borderRadius: false,
+  showTags: true,
+  showDate: true,
   openInNewTab: true,
+}
+
+function Container({ children }){
+  const container = {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center'
+  }
+  
+  const content = {
+    width: '80%',
+    height: 'max-content',
+  }
+
+  return (
+    <div style={container}>
+      <div style={content}>
+        {
+          children
+        }
+      </div>
+    </div>
+  )
 }
 
 export const Basic = (args) => <List {...args} />
@@ -20,6 +43,8 @@ Basic.args = {
   options,
   dataMedium: { disable: true }
 }
+
+export const Contained = () => <Container> <List username="alex.streza" /> </Container>
 
 export const Placeholder = (args) => <List {...args} />
 
