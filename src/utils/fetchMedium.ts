22c0cb13f86856199a32c49a202def69773200ee
David Fernando <davidfernandodamata21@gmail.com>
2022-08-30 23:00:14 +0000

    Update MediumPostsAPI url

--- src/utils/fetchMedium.ts
+++ src/utils/fetchMedium.ts
@@ -1,9 +1,9 @@
 const axios = require('axios')
 
 async function fetchMedium(username){
-  const baseUrl = 'https://mediumpostapi.herokuapp.com'
+  const baseUrl = 'https://mediumpostsapi.vercel.app'
 
-  const response = await axios.get(`${baseUrl}/?usermedium=${username}`)
+  const response = await axios.get(`${baseUrl}/api/${username}`)
   const data = response.data
   return data
 }
