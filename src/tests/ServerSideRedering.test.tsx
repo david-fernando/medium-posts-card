29565a8a31c14850231be0594be576e2237e5f57
David Fernando <davidfernandodamata21@gmail.com>
2022-09-08 23:09:12 +0000

    Update classCardContainer

--- src/tests/ServerSideRedering.test.tsx
+++ src/tests/ServerSideRedering.test.tsx
@@ -4,7 +4,7 @@ describe('Rendering test of components on the server', () => {
 
   test('Should rendering carousel on server', async() => {
 
-    const classCardContainer = 'Card_module_container__c1140499'
+    const classCardContainer = 'Card_module_container__bd43ad53'
     
     const test = await axios.get('http://localhost:3333/carousel')
     
@@ -15,7 +15,7 @@ describe('Rendering test of components on the server', () => {
 
   test('Should rendering list on server', async() => {
 
-    const classCardContainer = 'RectangularCard_module_container__581567c0'
+    const classCardContainer = 'RectangularCard_module_container__e8f8839b'
     
     const test = await axios.get('http://localhost:3333/list')
     
