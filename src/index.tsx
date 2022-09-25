b38d0df355aa0661d1f288dc57b9f3cfccd5c891
David Fernando <davidfernandodamata21@gmail.com>
2022-09-25 15:49:38 +0000

    Add Label

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import Carousel from "./components/Carousel";
 import List from "./components/List";
+import Label from "./components/Label";
 import fetchMedium from './utils/fetchMedium';
 
 export {
   Carousel,
   List,
+  Label,
   fetchMedium
 }
