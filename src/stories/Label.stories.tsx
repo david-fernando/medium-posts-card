5a357c9811e75cbf466fc959539df16d5fdf0c6d
David Fernando <davidfernandodamata21@gmail.com>
2022-09-25 15:49:18 +0000

    Add WrapList

--- src/stories/Label.stories.tsx
+++ src/stories/Label.stories.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Label from '../components/Label'
 import Carousel from '../components/Carousel'
+import List from '../components/List'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   component: Label
 }
 
-export const WrapCarousel = ()=> <Label> <Carousel username="alex.streza" /> </Label>
+export const WrapCarousel = (args)=> <Label {...args}> <Carousel username="alex.streza" /> </Label>
+export const WrapList = (args)=> <Label {...args}> <List username="alex.streza" /> </Label>
+
+WrapCarousel.args = {
+  text: 'Medium Articles'
+}
+
+WrapList.args = {
+  text: 'Medium Articles'
+}
